8e25ae6dbbcfb6ee84355f3c46d02abd0917b354
eruvalca <eruvalca@outlook.com>
2025-11-18 22:48:54 -0500

    Implement Disable2fa functionality with user management and logging

---
+++ Calcio/Calcio/Components/Account/Pages/Manage/Disable2fa.razor.cs
@@ -0,0 +1,58 @@
+using Calcio.Data;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Identity;
+
+namespace Calcio.Components.Account.Pages.Manage;
+
+public partial class Disable2fa(
+    UserManager<ApplicationUser> userManager,
+    IdentityRedirectManager redirectManager,
+    ILogger<Disable2fa> logger)
+{
+    private ApplicationUser? user;
+
+    [CascadingParameter]
+    private HttpContext HttpContext { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        user = await userManager.GetUserAsync(HttpContext.User);
+        if (user is null)
+        {
+            redirectManager.RedirectToInvalidUser(userManager, HttpContext);
+            return;
+        }
+
+        if (HttpMethods.IsGet(HttpContext.Request.Method) && !await userManager.GetTwoFactorEnabledAsync(user))
+        {
+            throw new InvalidOperationException("Cannot disable 2FA for user as it's not currently enabled.");
+        }
+    }
+
+    private async Task OnSubmitAsync()
+    {
+        if (user is null)
+        {
+            redirectManager.RedirectToInvalidUser(userManager, HttpContext);
+            return;
+        }
+
+        var disable2faResult = await userManager.SetTwoFactorEnabledAsync(user, false);
+        if (!disable2faResult.Succeeded)
+        {
+            throw new InvalidOperationException("Unexpected error occurred disabling 2FA.");
+        }
+
+        var userId = await userManager.GetUserIdAsync(user);
+        LogUserDisabled2fa(logger, userId);
+
+        redirectManager.RedirectToWithStatus(
+            "Account/Manage/TwoFactorAuthentication",
+            "2fa has been disabled. You can reenable 2fa when you setup an authenticator app",
+            HttpContext);
+    }
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "User with ID '{UserId}' has disabled 2fa.")]
+    private static partial void LogUserDisabled2fa(ILogger logger, string userId);
+}
