bee4bc9476a5b0034744427419fc5015ab7449b2
eruvalca <eruvalca@outlook.com>
2026-02-03 02:16:40 -0600

    feat: Enhance Home component to manage user clubs navigation based on authentication status

--- Calcio/Calcio/Components/Pages/Home.razor.cs
+++ Calcio/Calcio/Components/Pages/Home.razor.cs
@@ -1,5 +1,33 @@
+using Calcio.Shared.Services.Clubs;
+
+using Microsoft.AspNetCore.Components;
+
 namespace Calcio.Components.Pages;
 
-public partial class Home
+public partial class Home(
+    IClubsService clubsService,
+    NavigationManager navigationManager)
 {
+    [CascadingParameter]
+    private HttpContext HttpContext { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        if (HttpContext.User.Identity?.IsAuthenticated is not true)
+        {
+            return;
+        }
+
+        var userClubsResult = await clubsService.GetUserClubsAsync(CancellationToken);
+
+        userClubsResult.Switch(
+            clubs =>
+            {
+                if (clubs.Count == 0)
+                {
+                    navigationManager.NavigateTo("Account/Manage/Clubs");
+                }
+            },
+            _ => { }); // Fail open on service errors
+    }
 }
